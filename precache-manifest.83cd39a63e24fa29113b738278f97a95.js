6394ff22165eeac6428aa5a949e3edebfbad9635
AnthonyMuniz <53578013+AnthonyMuniz@users.noreply.github.com>
2020-01-05 11:34:57 -0600

    Add files via upload

---
+++ precache-manifest.83cd39a63e24fa29113b738278f97a95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cba647115d928d4434399f0fc0d313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "837060e81bdffa81353e",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5f22fdee42f179d87603",
+    "url": "/static/css/main.e8777027.chunk.css"
+  },
+  {
+    "revision": "837060e81bdffa81353e",
+    "url": "/static/js/2.495a994f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.495a994f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f22fdee42f179d87603",
+    "url": "/static/js/main.ce19c42b.chunk.js"
+  },
+  {
+    "revision": "01cfe46d64e7031e80ea",
+    "url": "/static/js/runtime-main.0a3b50ab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
